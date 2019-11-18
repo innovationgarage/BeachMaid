cd0ed6f506e23785b117e15e05cdb37c4eddf7cd
Erwin Ried <1091420+eried@users.noreply.github.com>
2019-11-18 09:49:06 +0100

    Some tests

---
+++ motion-sensor/MotionFlow/Player/MainForm.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Player
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( )
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.localVideoCaptureDeviceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openVideofileusingDirectShowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openJPEGURLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openMJPEGURLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.capture1stDisplayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.fpsLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBoxTemp = new System.Windows.Forms.PictureBox();
+            this.timerMouse = new System.Windows.Forms.Timer(this.components);
+            this.mainMenuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTemp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(927, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.localVideoCaptureDeviceToolStripMenuItem,
+            this.openVideofileusingDirectShowToolStripMenuItem,
+            this.openJPEGURLToolStripMenuItem,
+            this.openMJPEGURLToolStripMenuItem,
+            this.capture1stDisplayToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // localVideoCaptureDeviceToolStripMenuItem
+            // 
+            this.localVideoCaptureDeviceToolStripMenuItem.Name = "localVideoCaptureDeviceToolStripMenuItem";
+            this.localVideoCaptureDeviceToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.localVideoCaptureDeviceToolStripMenuItem.Text = "Local &Video Capture Device";
+            this.localVideoCaptureDeviceToolStripMenuItem.Click += new System.EventHandler(this.localVideoCaptureDeviceToolStripMenuItem_Click);
+            // 
+            // openVideofileusingDirectShowToolStripMenuItem
+            // 
+            this.openVideofileusingDirectShowToolStripMenuItem.Name = "openVideofileusingDirectShowToolStripMenuItem";
+            this.openVideofileusingDirectShowToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.openVideofileusingDirectShowToolStripMenuItem.Text = "Open video &file (using DirectShow)";
+            this.openVideofileusingDirectShowToolStripMenuItem.Click += new System.EventHandler(this.openVideofileusingDirectShowToolStripMenuItem_Click);
+            // 
+            // openJPEGURLToolStripMenuItem
+            // 
+            this.openJPEGURLToolStripMenuItem.Name = "openJPEGURLToolStripMenuItem";
+            this.openJPEGURLToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.openJPEGURLToolStripMenuItem.Text = "Open JPEG &URL";
+            this.openJPEGURLToolStripMenuItem.Click += new System.EventHandler(this.openJPEGURLToolStripMenuItem_Click);
+            // 
+            // openMJPEGURLToolStripMenuItem
+            // 
+            this.openMJPEGURLToolStripMenuItem.Name = "openMJPEGURLToolStripMenuItem";
+            this.openMJPEGURLToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.openMJPEGURLToolStripMenuItem.Text = "Open &MJPEG URL";
+            this.openMJPEGURLToolStripMenuItem.Click += new System.EventHandler(this.openMJPEGURLToolStripMenuItem_Click);
+            // 
+            // capture1stDisplayToolStripMenuItem
+            // 
+            this.capture1stDisplayToolStripMenuItem.Name = "capture1stDisplayToolStripMenuItem";
+            this.capture1stDisplayToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.capture1stDisplayToolStripMenuItem.Text = "Capture 1st display";
+            this.capture1stDisplayToolStripMenuItem.Click += new System.EventHandler(this.capture1stDisplayToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(254, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fpsLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 453);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(927, 22);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // fpsLabel
+            // 
+            this.fpsLabel.Name = "fpsLabel";
+            this.fpsLabel.Size = new System.Drawing.Size(912, 17);
+            this.fpsLabel.Spring = true;
+            this.fpsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // videoSourcePlayer
+            // 
+            this.videoSourcePlayer.AutoSizeControl = true;
+            this.videoSourcePlayer.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.videoSourcePlayer.ForeColor = System.Drawing.Color.White;
+            this.videoSourcePlayer.Location = new System.Drawing.Point(3, 3);
+            this.videoSourcePlayer.Name = "videoSourcePlayer";
+            this.videoSourcePlayer.Size = new System.Drawing.Size(322, 242);
+            this.videoSourcePlayer.TabIndex = 0;
+            this.videoSourcePlayer.VideoSource = null;
+            this.videoSourcePlayer.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer_NewFrame);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "AVI files (*.avi)|*.avi|All files (*.*)|*.*";
+            this.openFileDialog.Title = "Opem movie";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 410F));
+            this.tableLayoutPanel1.Controls.Add(this.videoSourcePlayer, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBoxTemp, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(927, 429);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // pictureBoxTemp
+            // 
+            this.pictureBoxTemp.Location = new System.Drawing.Point(520, 3);
+            this.pictureBoxTemp.Name = "pictureBoxTemp";
+            this.pictureBoxTemp.Size = new System.Drawing.Size(353, 265);
+            this.pictureBoxTemp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxTemp.TabIndex = 1;
+            this.pictureBoxTemp.TabStop = false;
+            // 
+            // timerMouse
+            // 
+            this.timerMouse.Enabled = true;
+            this.timerMouse.Tick += new System.EventHandler(this.timerMouse_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 475);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "MainForm";
+            this.Text = "Simple Player";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTemp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem localVideoCaptureDeviceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
+        private System.Windows.Forms.ToolStripStatusLabel fpsLabel;
+        private System.Windows.Forms.ToolStripMenuItem openVideofileusingDirectShowToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ToolStripMenuItem openJPEGURLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openMJPEGURLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem capture1stDisplayToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBoxTemp;
+        private System.Windows.Forms.Timer timerMouse;
+    }
+}
+
---
+++ motion-sensor/MotionFlow/Player/MainForm.cs
@@ -0,0 +1,338 @@
+﻿// Simple Player sample application
+// AForge.NET framework
+// http://www.aforgenet.com/framework/
+//
+// Copyright © AForge.NET, 2006-2011
+// contacts@aforgenet.com
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace Player
+{
+    public partial class MainForm : Form
+    {
+        byte[,] previous = null;
+        private int lastx;
+        private int lasty;
+
+        // Class constructor
+        public MainForm( )
+        {
+            InitializeComponent( );
+        }
+
+        private void MainForm_FormClosing( object sender, FormClosingEventArgs e )
+        {
+            CloseCurrentVideoSource( );
+        }
+
+        // "Exit" menu item clicked
+        private void exitToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            this.Close( );
+        }
+
+        // Open local video capture device
+        private void localVideoCaptureDeviceToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            VideoCaptureDeviceForm form = new VideoCaptureDeviceForm( );
+
+            if ( form.ShowDialog( this ) == DialogResult.OK )
+            {
+                // create video source
+                VideoCaptureDevice videoSource = form.VideoDevice;
+
+                // open it
+                OpenVideoSource( videoSource );
+            }
+        }
+
+        // Open video file using DirectShow
+        private void openVideofileusingDirectShowToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            if ( openFileDialog.ShowDialog( ) == DialogResult.OK )
+            {
+                // create video source
+                FileVideoSource fileSource = new FileVideoSource( openFileDialog.FileName );
+
+                // open it
+                OpenVideoSource( fileSource );
+            }
+        }
+
+        // Open JPEG URL
+        private void openJPEGURLToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            URLForm form = new URLForm( );
+
+            form.Description = "Enter URL of an updating JPEG from a web camera:";
+            form.URLs = new string[]
+				{
+					"http://195.243.185.195/axis-cgi/jpg/image.cgi?camera=1",
+				};
+
+            if ( form.ShowDialog( this ) == DialogResult.OK )
+            {
+                // create video source
+                JPEGStream jpegSource = new JPEGStream( form.URL );
+
+                // open it
+                OpenVideoSource( jpegSource );
+            }
+        }
+
+        // Open MJPEG URL
+        private void openMJPEGURLToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            URLForm form = new URLForm( );
+
+            form.Description = "Enter URL of an MJPEG video stream:";
+            form.URLs = new string[]
+				{
+					"http://195.243.185.195/axis-cgi/mjpg/video.cgi?camera=4",
+					"http://195.243.185.195/axis-cgi/mjpg/video.cgi?camera=3",
+				};
+
+            if ( form.ShowDialog( this ) == DialogResult.OK )
+            {
+                // create video source
+                MJPEGStream mjpegSource = new MJPEGStream( form.URL );
+
+                // open it
+                OpenVideoSource( mjpegSource );
+            }
+        }
+
+        // Capture 1st display in the system
+        private void capture1stDisplayToolStripMenuItem_Click( object sender, EventArgs e )
+        {
+            OpenVideoSource( new ScreenCaptureStream( Screen.AllScreens[0].Bounds, 100 ) );
+        }
+
+        // Open video source
+        private void OpenVideoSource( IVideoSource source )
+        {
+            // set busy cursor
+            this.Cursor = Cursors.WaitCursor;
+
+            // stop current video source
+            CloseCurrentVideoSource( );
+
+            // start new video source
+            videoSourcePlayer.VideoSource = source;
+            videoSourcePlayer.Start( );
+
+
+            this.Cursor = Cursors.Default;
+        }
+
+        // Close video source if it is running
+        private void CloseCurrentVideoSource( )
+        {
+            if ( videoSourcePlayer.VideoSource != null )
+            {
+                videoSourcePlayer.SignalToStop( );
+
+                // wait ~ 3 seconds
+                for ( int i = 0; i < 30; i++ )
+                {
+                    if ( !videoSourcePlayer.IsRunning )
+                        break;
+                    System.Threading.Thread.Sleep( 100 );
+                }
+
+                if ( videoSourcePlayer.IsRunning )
+                {
+                    videoSourcePlayer.Stop( );
+                }
+
+                videoSourcePlayer.VideoSource = null;
+            }
+        }
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighSpeed;
+                graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+                graphics.SmoothingMode = SmoothingMode.None;
+                graphics.PixelOffsetMode = PixelOffsetMode.None;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+        // New frame received by the player
+        private void videoSourcePlayer_NewFrame(object sender, ref Bitmap image)
+        {
+            /*DateTime now = DateTime.Now;
+            Graphics g = Graphics.FromImage( image );
+
+            // paint current time
+            SolidBrush brush = new SolidBrush( Color.Red );
+            g.DrawString( now.ToString( ), this.Font, brush, new PointF( 5, 5 ) );
+            brush.Dispose( );
+
+            g.Dispose( );*/
+
+            const int size = 15;
+            const int sign_size = 2;
+            Bitmap temp = ResizeImage(new Bitmap((Image)image.Clone()), size, size);
+
+ 
+            // Create matrix
+            var current = new byte[size, size];
+
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    current[x, y] = ConvertToByte(temp.GetPixel(x, y));
+            //previous = new int[size, size];
+
+            var g = Graphics.FromImage(temp);
+
+            const int rectangleSize = 2;
+            var rx = (size + rectangleSize) / 2;
+            var ry = rx;
+
+            if (previous == null)
+            {
+
+
+
+            }
+            else
+            {
+                // Find previous in current
+
+                switch (GetDirection(current, previous))
+                {
+                    case ContentAlignment.TopLeft: rx = 0; ry = 0; break;
+                    case ContentAlignment.TopCenter: ry = 0; break;
+                    case ContentAlignment.TopRight: rx = size- rectangleSize; ry = 0; break;
+                    case ContentAlignment.MiddleLeft: rx = 0; break;
+                    case ContentAlignment.MiddleRight: rx = size- rectangleSize; break;
+                    case ContentAlignment.BottomLeft: rx = 0; ry = size - rectangleSize; break;
+                    case ContentAlignment.BottomCenter: ry = size - rectangleSize; break;
+                    case ContentAlignment.BottomRight: rx = size - rectangleSize; ry = size - rectangleSize; break;
+                }
+            }
+
+            lasty= rx- (size + rectangleSize) / 2;
+            lastx = ry- (size + rectangleSize) / 2;
+
+            g.FillRectangle(Brushes.Red, rx, ry, rectangleSize, rectangleSize);
+            previous = current;
+            g.Dispose();
+
+            pictureBoxTemp.Image = temp;
+
+        }
+
+        private void MoveCursor(int x, int y)
+        {
+            // Set the Current cursor, move the cursor's Position,
+            // and set its clipping rectangle to the form. 
+
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(Cursor.Position.X - x, Cursor.Position.Y - y);
+            Cursor.Clip = new Rectangle(this.Location, this.Size);
+        }
+
+        private ContentAlignment GetDirection(byte[,] current, byte[,] previous)
+        {
+            int bestx = 0, besty = 0, bestscore = int.MaxValue, size = current.GetLength(0);
+            const int maxoffset = 1;
+            for (int x = -maxoffset; x <= maxoffset; x++)
+                for (int y = -maxoffset; y <= maxoffset; y++)
+                {
+                    int score = 0;
+
+                    for (int i = maxoffset; i < size - maxoffset; i++)
+                        for (int j = maxoffset; j < size - maxoffset; j++)
+                        {
+                            score += Math.Abs(previous[i, j] - current[i+x, j+y]);
+                            Debug.WriteLine("Comparing " + i + "," + j + " to " + (i + x) + " " + (j + y));
+                        }
+
+                    if (score < bestscore || (x==0 && y==0 && score <= bestscore))
+                    {
+                        bestx = x;
+                        besty = y;
+                        bestscore = score;
+                    }
+
+                    /*if (score == 0)
+                        break; // Perfect match*/
+                }
+
+            switch (bestx)
+            {
+                case -1:
+                    switch (besty)
+                    {
+                        case -1: return ContentAlignment.TopLeft;
+                        case 0: return ContentAlignment.TopCenter;
+                        case 1: return ContentAlignment.TopRight;
+
+                    }
+                    break;
+
+                case 0:
+                    switch (besty)
+                    {
+                        case -1: return ContentAlignment.MiddleLeft;
+                        case 0: return ContentAlignment.MiddleCenter;
+                        case 1: return ContentAlignment.MiddleRight;
+
+                    }
+                    break;
+
+                case 1:
+                    switch (besty)
+                    {
+                        case -1: return ContentAlignment.BottomLeft;
+                        case 0: return ContentAlignment.BottomCenter;
+                        case 1: return ContentAlignment.BottomRight;
+
+                    }
+                    break;
+            }
+            return ContentAlignment.MiddleCenter;
+        }
+
+        private byte ConvertToByte(Color color)
+        {
+            return (byte)((color.R + color.B + color.G) / 3);
+        }
+
+        private void timerMouse_Tick(object sender, EventArgs e)
+        {
+            if(videoSourcePlayer.VideoSource !=null && videoSourcePlayer.VideoSource.IsRunning)
+                MoveCursor(lastx, lasty);
+        }
+    }
+}
