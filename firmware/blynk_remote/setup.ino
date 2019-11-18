f7c1f2cdd121eff6a836c583fe81a6f195e97a61
Erwin Ried <1091420+eried@users.noreply.github.com>
2019-11-18 10:26:17 +0100

    Update setup.ino

--- firmware/blynk_remote/setup.ino
+++ firmware/blynk_remote/setup.ino
@@ -1,37 +1,37 @@
 void setup() {
-  Serial.begin(115200);
-  cli.setOnError(errorCallback); // Set error Callback
-  stopCmd = cli.addCmd("stop", stop_callback);
-  beepCmd = cli.addCmd("beep", beep_callback);
-  rebootCmd = cli.addCmd("reboot", reboot_callback);
-  helpCmd = cli.addCmd("help", help_callback);
-  statusCmd = cli.addCmd("status", status_callback);
+  /*Serial.begin(115200);
+    cli.setOnError(errorCallback); // Set error Callback
+    stopCmd = cli.addCmd("stop", stop_callback);
+    beepCmd = cli.addCmd("beep", beep_callback);
+    rebootCmd = cli.addCmd("reboot", reboot_callback);
+    helpCmd = cli.addCmd("help", help_callback);
+    statusCmd = cli.addCmd("status", status_callback);
 
-  moveCmd = cli.addCmd("move", move_callback);
-  moveCmd.addPosArg("x");
-  moveCmd.addPosArg("y");
-  moveCmd.addPosArg("smoothing", "1");
-  beltsCmd = cli.addCmd("belts", belts_callback);
-  beltsCmd.addPosArg("left");
-  beltsCmd.addPosArg("right");
-  beltsCmd.addPosArg("smoothing", "1");
-  rebootCmd.addFlagArg("force");
-  beepCmd.addPosArg("melody", "0");
-  statusCmd.addFlagArg("stream");
-  statusCmd.addFlagArg("quiet");
+    moveCmd = cli.addCmd("move", move_callback);
+    moveCmd.addPosArg("x");
+    moveCmd.addPosArg("y");
+    moveCmd.addPosArg("smoothing", "1");
+    beltsCmd = cli.addCmd("belts", belts_callback);
+    beltsCmd.addPosArg("left");
+    beltsCmd.addPosArg("right");
+    beltsCmd.addPosArg("smoothing", "1");
+    rebootCmd.addFlagArg("force");
+    beepCmd.addPosArg("melody", "0");
+    statusCmd.addFlagArg("stream");
+    statusCmd.addFlagArg("quiet");*/
 
   beep();
 
-  wifiMulti.addAP(ssid1, pass1);
-  wifiMulti.addAP(ssid2, pass2);
-  wifiMulti.addAP(ssid3, pass3);
+  /*wifiMulti.addAP(ssid1, pass1);
+    wifiMulti.addAP(ssid2, pass2);
+    wifiMulti.addAP(ssid3, pass3);
 
-  WiFi.hostname(machine_name);
-  WiFi.mode(WIFI_STA);
+    WiFi.hostname(machine_name);
+    WiFi.mode(WIFI_STA);
 
-  Blynk.config(auth, server, server_port);  // I am using the local Server
-  check_connection();// It needs to run first to initiate the connection. Same function works for checking the connection!
-
+    Blynk.config(auth, server, server_port);  // I am using the local Server
+    check_connection();// It needs to run first to initiate the connection. Same function works for checking the connection!
+  */
   speed_controller.setInterval(100L, check_and_set_speed);
   system_status.setInterval(1000L, report_status);
 
@@ -39,12 +39,25 @@ void setup() {
   setMotorPins(leftEngine);
   setMotorPins(rightEngine);
 
-  if(Blynk.connected())
-  play_melody(10); // Boot
-  else
+  /*if (Blynk.connected())
+    play_melody(10); // Boot
+    else*/
   {
     offline_mode = true;
     play_melody(11);
   }
   boot_msg(true);
+
+  // Keep me alive
+  smoothing = 2000;
+  while (true)
+  {
+    delay(1000);
+    beep();
+    delay(1000);
+    setBothMotorsSpeed(100, 100);
+    delay(8000);
+    setBothMotorsSpeed(0, 0);
+    delay(45000);
+  }
 }
